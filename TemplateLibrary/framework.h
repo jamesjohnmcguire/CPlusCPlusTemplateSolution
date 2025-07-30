1547eb8047e059619a506ab35a332d4352ec24d7
James John McGuire <jamesjohnmcguire@gmail.com>
2025-07-30 21:49:18 +0900

    Optimize Header Files

--- TemplateLibrary/framework.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef _WIN32
-	// Exclude rarely-used stuff from Windows headers
-	#define WIN32_LEAN_AND_MEAN
-	#include <Windows.h>
-#endif
--- TemplateLibrary/pch.h
+++ TemplateLibrary/pch.h
@@ -1,6 +1,17 @@
 #pragma once
 
-#include "framework.h"
+#ifdef _WIN32
+	// Allow use of features specific to Windows 7 or later.
+	#define WINVER 0x0601
+	#define _WIN32_WINNT 0x0601
+
+	// Exclude rarely-used stuff from Windows headers
+	#define WIN32_LEAN_AND_MEAN
+	#define VC_EXTRALEAN
+
+	#include <Shlobj.h>
+	#include <Windows.h>
+#endif
 
 // Add cross-platform headers here
 #include <iostream>
